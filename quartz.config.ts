a87e962db155f6766ada98301d3037821ae6bc0a
Lumen Yang <lumen.yang@lumeny.io>
2024-10-21 23:41:35 +0200

    chore: unicode update

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🕯 Lumen's Notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
