842febd9fce9a832fa570c2de5653e18ebad91dc
Lumen Yang <lumen.yang@lumeny.io>
2024-10-22 15:51:26 +0200

    Quartz sync: Oct 22, 2024, 3:51 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🕯 Lumen's Notes",
+    pageTitle: "🌟 Lumen's Notes",
     pageTitleSuffix: "Live my live consciously",
     enableSPA: true,
     enablePopovers: true,
