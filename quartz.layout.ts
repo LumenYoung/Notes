4b99d6f7deac8fa2a260c766b32d297e0a85b126
Lumen Yang <lumen.yang@lumeny.io>
2024-11-04 23:51:36 +0100

    fix

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -9,8 +9,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      MyGitHub: "https://github.com/LumenYoung",
     },
   }),
 }
