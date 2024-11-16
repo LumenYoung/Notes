e3253107fe07c70dd68dcf36bf9841210217174e
Lumen Yang <lumen.yang@lumeny.io>
2024-11-16 23:05:35 +0100

    misc

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -78,7 +78,7 @@ export const defaultContentPageLayout: PageLayout = {
         categoryId: "DIC_kwDONDxfsM4Cj7Qu",
         mapping: "pathname",
         reactionsEnabled: true,
-        inputPosition: "bottom",
+        inputPosition: "top",
       },
     }),
   ],
