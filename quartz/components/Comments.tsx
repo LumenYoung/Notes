4d35ac150a4cc2d9b137291b8e186f3d8213860c
Lumen Yang <lumen.yang@lumeny.io>
2024-11-17 17:49:08 +0100

    fix: comment section

--- quartz/components/Comments.tsx
+++ quartz/components/Comments.tsx
@@ -27,8 +27,7 @@ function boolToStringBool(b: boolean): string {
 export default ((opts: Options) => {
   const Comments: QuartzComponent = ({ displayClass, fileData, cfg }: QuartzComponentProps) => {
     // check if comments should be displayed according to frontmatter
-    const disableComment: boolean =
-      !fileData.frontmatter?.comments || fileData.frontmatter?.comments === "false"
+    const disableComment: boolean = fileData.frontmatter?.comments === "false"
     if (disableComment) {
       return <></>
     }
