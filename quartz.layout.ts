358544fa7eb75abd185169984357e2acdde51e34
Lumen Yang <lumen.yang@lumeny.io>
2024-11-19 23:13:47 +0100

    layout: recent note number tweak

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -46,7 +46,7 @@ export const defaultContentPageLayout: PageLayout = {
           endsWith(slug, "index.html")
         )
       },
-      limit: 4,
+      limit: 5,
       showTags: true,
     }),
     Component.Comments({
@@ -106,7 +106,7 @@ export const defaultListPageLayout: PageLayout = {
           endsWith(slug, "index.html")
         )
       },
-      limit: 4,
+      limit: 5,
       showTags: true,
     }),
   ],
