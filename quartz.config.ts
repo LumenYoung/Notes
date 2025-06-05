69ab6abb7111bfe0f70769918b4f60eed8341d5d
Lumen Yang <lumen.yang@lumeny.io>
2025-06-05 01:11:28 +0000

    Minor change, remove emoji

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "💡 Lumen's Notes. ", // 🌟
+    pageTitle: "Lumen's Notes. ", // 🌟
     pageTitleSuffix: " ☁️ Live my life consciously",
     enableSPA: true,
     enablePopovers: true,
