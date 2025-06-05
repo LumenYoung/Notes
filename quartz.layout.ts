ff478bd9b33fdd1ad72514002e25115c2c090fe8
Lumen Yang <lumen.yang@lumeny.io>
2025-06-05 13:56:16 +0000

    Revert "site: move recent in Page body after comments"
    This reverts commit 4f33ff02c4b33697ed6b46bfffe82ca901737925.

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -62,6 +62,19 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Backlinks(),
   ],
   afterBody: [
+    Component.RecentNotes({
+      filter: (file) => {
+        const slug = file.slug ?? ""
+        return !(
+          slug.endsWith("/") ||
+          endsWith(slug, "index") ||
+          endsWith(slug, "index.md") ||
+          endsWith(slug, "index.html")
+        )
+      },
+      limit: 5,
+      showTags: true,
+    }),
     Component.Comments({
       provider: "giscus",
       options: {
@@ -77,19 +90,6 @@ export const defaultContentPageLayout: PageLayout = {
         reactionsEnabled: true,
         inputPosition: "top",
       },
-    }),
-    Component.RecentNotes({
-      filter: (file) => {
-        const slug = file.slug ?? ""
-        return !(
-          slug.endsWith("/") ||
-          endsWith(slug, "index") ||
-          endsWith(slug, "index.md") ||
-          endsWith(slug, "index.html")
-        )
-      },
-      limit: 5,
-      showTags: true,
     }),
   ],
 }
